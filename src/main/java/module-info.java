8edfc145656b052c53f19916c6a459e4c8ea80b3
bellag10 <bellagriffith513@gmail.com>
2025-11-09 13:21:40 -0600

    removed comment and module-info

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.utsa.cs3443.macromateapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens edu.utsa.cs3443.macromateapp to javafx.fxml;
-    exports edu.utsa.cs3443.macromateapp;
-}
