7e11ba46bf86d5ba0c0a5e89be95e2009e1856f0
mmd531 <mmdavis531@gmail.com>
2025-11-09 08:18:19 -0600

    Update HelloApplication.java
    I added a comment

--- src/main/java/edu/utsa/cs3443/macromateapp/HelloApplication.java
+++ src/main/java/edu/utsa/cs3443/macromateapp/HelloApplication.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+//writing a comment as a test
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
